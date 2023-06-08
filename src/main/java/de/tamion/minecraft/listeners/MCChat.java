73a84b43d4aeeb6f9a47f2fe945478d25319b00e
Tamion <simon.dorner.obf@gmail.com>
2023-06-08 20:26:45 +0200

    Only send to discord if event is not canceled - Fixed Exception where jda was null

--- src/main/java/de/tamion/minecraft/listeners/MCChat.java
+++ src/main/java/de/tamion/minecraft/listeners/MCChat.java
@@ -8,13 +8,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 
-import java.util.HashMap;
-
 public class MCChat implements Listener {
      @EventHandler
      public void onChat(PlayerChatEvent e) {
          Player p = e.getPlayer();
          FileConfiguration config = MCMain.getPlugin().getConfig();
-         Utils.sendtochat(config.getString("Bot.dcsyntax").replaceAll("\\{username}", e.getPlayer().getName()).replaceAll("\\{message}", e.getMessage()));
+         if(!e.isCancelled()) {
+             Utils.sendtochat(config.getString("Bot.dcsyntax").replaceAll("\\{username}", e.getPlayer().getName()).replaceAll("\\{message}", e.getMessage()));
+         }
      }
 }
