1d8c7f20989b0723c3b6fd67df0241ed026923eb
Tamion <70228790+notTamion@users.noreply.github.com>
2023-09-30 10:17:51 +0200

    Added api-version, Switched to AsyncChatEvent

--- src/main/java/de/tamion/minecraft/listeners/MCChat.java
+++ src/main/java/de/tamion/minecraft/listeners/MCChat.java
@@ -2,19 +2,20 @@ package de.tamion.minecraft.listeners;
 
 import de.tamion.minecraft.MCMain;
 import de.tamion.others.Utils;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
 
 public class MCChat implements Listener {
      @EventHandler
-     public void onChat(PlayerChatEvent e) {
+     public void onChat(AsyncChatEvent e) {
          Player p = e.getPlayer();
          FileConfiguration config = MCMain.getPlugin().getConfig();
          if(!e.isCancelled()) {
-             Utils.sendtochat(config.getString("Bot.dcsyntax").replaceAll("\\{username}", e.getPlayer().getName()).replaceAll("\\{message}", e.getMessage()));
+             Utils.sendtochat(config.getString("Bot.dcsyntax").replaceAll("\\{username}", e.getPlayer().getName()).replaceAll("\\{message}", PlainTextComponentSerializer.plainText().serialize(e.message())));
          }
      }
 }
